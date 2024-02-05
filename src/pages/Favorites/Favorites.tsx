be286a5503e7601431583c4178a484a9d1f584b3
Monica Stoyanova <monicastoyanova1@gmail.com>
2024-02-05 14:54:38 +0200

    Favorites can now be removed from within Favorites list

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,19 +1,26 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
+import { RootState, useAppDispatch } from "../../store/store";
 
 import { Beer } from "../../utils/consts";
 
 import BeerCard from "../../components/BeerCard/BeerCard";
 
 import styles from './Favorites.module.css'
+import { toggleFavorite } from "../../store/slices/favoritesSlice";
 
 const Favorites = () => {
     const favorites: Beer[] = useSelector((state: RootState) => state.favorites.favorites);
+    const dispatch = useAppDispatch();
 
     const renderBeerCards = () => {
         return (
             favorites.map(favorite => (
-                <BeerCard key={favorite.id} beer={favorite} />
+                <BeerCard
+                    key={favorite.id}
+                    beer={favorite}
+                    isFavorite={true}
+                    toggleFavorite={() => dispatch(toggleFavorite(favorite))}
+                />
             ))
         );
     }
