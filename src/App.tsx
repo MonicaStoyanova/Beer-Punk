eb5ad903bdfb168b72c7de9b4091f5ef0db3fc96
Monica Stoyanova <monicastoyanova1@gmail.com>
2024-02-05 17:42:16 +0200

    Metamask Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import BeerList from './pages/BeerList/BeerList';
 import Favorites from './pages/Favorites/Favorites';
@@ -11,25 +12,36 @@ import Footer from './components/Footer/Footer';
 import { store } from './store/store'
 
 import './App.css';
+import { useEffect, useState } from 'react';
 
 
 function App() {
-
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  useEffect(() => {
+    const account = localStorage.getItem('defaultAccount');
+    setIsLoggedIn(!!account);
+  }, []);
 
   return (
     <>
       <Provider store={store}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<BeerList />} />
-          <Route path="/favorite" element={<Favorites />} />
-          <Route path="/random-beer" element={<RandomBeer />} />
-          <Route path="/logout" element={<Logout />} />
-        </Routes>
-        <Footer />
+        {!isLoggedIn ? (
+          <Login onLogin={() => setIsLoggedIn(true)} />
+        ) : (
+          <>
+            <Header />
+            <Routes>
+              <Route path="/" element={<BeerList />} />
+              <Route path="/favorite" element={<Favorites />} />
+              <Route path="/random-beer" element={<RandomBeer />} />
+              <Route path="/logout" element={<Logout />} />
+            </Routes>
+            <Footer />
+          </>
+        )}
       </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,50 @@
+import { ethers } from "ethers";
+import { useState } from "react";
+import styles from "./Login.module.css"
+declare global {
+    interface Window {
+        ethereum: any;
+    }
+}
+
+type LoginProps = {
+    onLogin: () => void;
+};
+
+const Login = ({ onLogin }: LoginProps) => {
+
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    const [defaultAccount, setDefaultAccount] = useState<string>("None")
+
+
+    const connectWallet = () => {
+
+        if (window.ethereum) {
+            window.ethereum.request({ method: "eth_requestAccounts" })
+                .then((result: string[]) => {
+                    accountChanged([result[0]])
+                    onLogin();
+                })
+                .catch((error: Error) => {
+                    setErrorMessage(error.message);
+                });
+        } else {
+            setErrorMessage('Install Metamask please')
+        }
+    }
+
+    const accountChanged = (accountName: string) => {
+        setDefaultAccount(accountName);
+        localStorage.setItem('defaultAccount', accountName);
+    }
+    return (
+        <div className={styles.metaMaskContainer}>
+            <h1 className={styles.title}>Metamask Wallet Connection</h1>
+            <button className={styles.connectBtn} onClick={connectWallet}>Connect wallet</button>
+            <h3 className={styles.user}>Address connected:   {defaultAccount}</h3>
+            <h3 className={styles.errorMessage}>{errorMessage}</h3>
+        </div>
+    )
+}
+
+export default Login
