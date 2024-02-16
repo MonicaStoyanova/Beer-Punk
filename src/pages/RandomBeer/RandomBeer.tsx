b51d8448eb878724728971d8ea94defbced4cc8d
Monica Stoyanova <monicastoyanova1@gmail.com>
2024-02-16 12:58:26 +0200

    Clicking Random Beer in the navigation, fetches new data

--- src/pages/RandomBeer/RandomBeer.tsx
+++ src/pages/RandomBeer/RandomBeer.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 
 import { useSelector } from "react-redux";
+
 import { RootState } from "../../store/store";
 import { fetchRandomBeer, resetRandomBeer } from "../../store/slices/beersSlice";
 import { useAppDispatch } from "../../store/store";
-
+import sound from "../../assets/beer-open.mp3"
+import useAudioPlayer from "../../hooks/useAudioPlayer";
 import { Beer } from "../../utils/consts";
 
 import BeerCard from "../../components/BeerCard/BeerCard";
@@ -12,20 +14,15 @@ import BeerCard from "../../components/BeerCard/BeerCard";
 import styles from './RandomBeer.module.css'
 
 const RandomBeer = () => {
-    const dispatch = useAppDispatch();
 
-
-    useEffect(() => {
-        dispatch(resetRandomBeer())
-        dispatch(fetchRandomBeer())
-    }, []);
+    const { playAudio } = useAudioPlayer(sound);
 
     const randomBeer: Beer[] = useSelector((state: RootState) => state.beers.randomBeer);
 
     const renderBeerCard = () => {
         return (
             randomBeer.map(random => (
-                <BeerCard key={random.id} beer={random} />
+                <BeerCard key={random.id} beer={random} playAudio={playAudio} />
             ))
         );
     }
